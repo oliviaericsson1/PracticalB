5aa75b2167abea2e49059e0b1ce42518f49e54d5
Julian <122232012+juliang72@users.noreply.github.com>
2025-03-18 15:46:24 -0400

    Update driver_script.py

--- src/driver_script.py
+++ src/driver_script.py
@@ -8,4 +8,4 @@ embedding_models = {"MiniLM": SentenceTransformer("all-MiniLM-L6-v2"),
 # Vector DBs
 redis_client = redis.Redis(host="localhost", port=6379, db=0) # Redis 
 chroma_client = chromadb.PersistentClient(path="./chroma_db") # Chroma
-# 1 Additional vector database
+
