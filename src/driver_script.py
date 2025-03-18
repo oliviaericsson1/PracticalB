b1c5636bc508f9a474750f9ecd3c333020cd7e47
Julian <122232012+juliang72@users.noreply.github.com>
2025-03-18 15:48:20 -0400

    Update driver_script.py

--- src/driver_script.py
+++ src/driver_script.py
@@ -1,3 +1,4 @@
+import chrombadb
 
 # Embedding Models
 embedding_models = {"MiniLM": SentenceTransformer("all-MiniLM-L6-v2"), 
@@ -6,6 +7,12 @@ embedding_models = {"MiniLM": SentenceTransformer("all-MiniLM-L6-v2"),
                    }
 
 # Vector DBs
-redis_client = redis.Redis(host="localhost", port=6379, db=0) # Redis 
-chroma_client = chromadb.PersistentClient(path="./chroma_db") # Chroma
+# Redis
+redis_client = redis.Redis(host="localhost", port=6379, db=0) 
+VECTOR_DIM = 768
+INDEX_NAME = "embedding_index"
+
+# Chroma
+chroma_client = chromadb.PersistentClient(path="./chroma_db")
+collection = chroma_client.get_or_create_collection(name="test_collection")
 
