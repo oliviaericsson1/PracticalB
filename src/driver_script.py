9dd382b9b0364aa49bf5cd09e77a8fa724f680fc
Julian <122232012+juliang72@users.noreply.github.com>
2025-03-18 15:42:20 -0400

    Update driver_script.py

--- src/driver_script.py
+++ src/driver_script.py
@@ -1,4 +1,7 @@
 
+# Embedding Models
 
-redis_client = redis.Redis(host="localhost", port=6379, db=0)
-chroma_client = chromadb.PersistentClient(path="./chroma_db")
+# Vector DBs
+redis_client = redis.Redis(host="localhost", port=6379, db=0) # Redis 
+chroma_client = chromadb.PersistentClient(path="./chroma_db") # Chroma
+# 1 Additional vector database
