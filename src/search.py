091180d63cbe28e6e6b75248a0db31a240a7b991
oliviaericsson1 <105184236+oliviaericsson1@users.noreply.github.com>
2025-03-19 11:51:22 -0400

    Update search.py

--- src/search.py
+++ src/search.py
@@ -9,7 +9,7 @@ from redis.commands.search.field import VectorField, TextField
 
 # Initialize models
 # embedding_model = SentenceTransformer("all-MiniLM-L6-v2")
-redis_client = redis.StrictRedis(host="localhost", port=6380, decode_responses=True)
+redis_client = redis.StrictRedis(host="localhost", port=6379, decode_responses=True)
 
 VECTOR_DIM = 768
 INDEX_NAME = "embedding_index"
